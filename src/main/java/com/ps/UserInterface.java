40f2c300c275dcb277c3136d23c542f86a78da71
Syed Haider <alihaidersyed101@gmail.com>
2024-05-07 08:48:34 -0400

    First Commit

---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,14 @@
+package com.ps;
+
+public class UserInterface {
+    //Chance Any combination Sum of All dice 11345
+
+    // Dices
+
+    // --Devise a plan--
+
+    // Responsibilities
+
+    //UserInterface
+    //Game
+}
