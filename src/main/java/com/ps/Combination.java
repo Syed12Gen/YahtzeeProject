a5d914921a8e86bd96c37b623b13269fc1829dc0
Syed Haider <alihaidersyed101@gmail.com>
2024-05-09 12:04:16 -0400

    combination class added where score be calculated

---
+++ src/main/java/com/ps/Combination.java
@@ -0,0 +1,50 @@
+package com.ps;
+
+import java.util.ArrayList;
+
+public class Combination {
+
+    private String name;
+
+    private String section;
+
+    public int calculateScore(ArrayList dice) {
+
+        switch (this.name) {
+
+            case "ones":
+                break;
+            case "twos":
+                break;
+            case "thres":
+                break;
+            case "fours":
+                break;
+            case "fives":
+                break;
+            case "sixes":
+                break;
+            case "sum":
+                break;
+            case "bonus":
+                break;
+            case "threeOfKind":
+                break;
+            case "fourOfKind":
+                break;
+            case "fullHouse":
+                break;
+            case "smallStraight":
+                break;
+            case "largeStraight":
+                break;
+            case "chance":
+                break;
+            case "yahtzee":
+                break;
+
+
+
+        }
+    }
+}
